2c7848cb5413f9babe845133266df17eedc24c9b
yrahhaou <yassinerahhaoui12@gmail.com>
2026-06-05 22:48:18 +0100

    update project structure

--- src/http/ParserState.java
+++ src/http/ParserState.java
@@ -1,1 +1,12 @@
 package http;
+
+/**
+ * Tracks the current state of the HTTP parser when reading asynchronous network data.
+ */
+public enum ParserState {
+    READING_REQUEST_LINE,
+    READING_HEADERS,
+    READING_BODY,
+    DONE,
+    ERROR
+}
